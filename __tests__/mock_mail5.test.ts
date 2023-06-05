8e13de0cce435ded20250fc05223e650f72a513a
Robin <robin.noormets@voco.ee>
2023-06-06 00:36:34 +0300

    mock tests

--- __tests__/mock_mail5.test.ts
+++ __tests__/mock_mail5.test.ts
@@ -3,3 +3,17 @@ import {initApp} from "../mock_mail5";
 test('start', () => {
     expect(initApp(200000, console.log)).toBe(true);
 });
+
+
+test('mock memory ok', () => {
+    let f=jest.fn();
+    initApp(200000, f);
+    expect(f).toBeCalledWith("Rakendus käivitus");
+});
+
+test('mock memory little', () => {
+    let f=jest.fn();
+    initApp(20000, f);
+    expect(f).toBeCalledWith("Vaba mälu ainult 20000");
+    expect(f).toBeCalledTimes(1);
+});
--- mock_mail5.ts
+++ mock_mail5.ts
@@ -1,7 +1,12 @@
-export function initApp(freeMemory: number,  mailFn: any): boolean{
-    if(freeMemory<100000){mailFn("Vaba mälu ainult "+freeMemory); return false;}
-    mailFn("Rakendus käivitus");
+export function initApp(freeMemory: number, mailFn: any): boolean {
+    if (freeMemory < 100000) {
+        mailFn('Vaba mälu ainult ' + freeMemory);
+        return false;
+    }
+    mailFn('Rakendus käivitus');
     return true;
 }
 
-initApp(1500000,  console.log);
+export function fakeInit() {
+    console.log('Käivitus kell ' + new Date());
+}
