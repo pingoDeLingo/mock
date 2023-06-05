4090a2b06f963dc621cc4abdf2de19febc4bf85b
Robin <robin.noormets@voco.ee>
2023-06-06 00:21:24 +0300

    Mitmekülgsem realiseerimine

--- adder.ts
+++ adder.ts
@@ -1,29 +1,47 @@
-interface Adder{
-    add(nr: number):void;
-    getSum():number;
+interface Adder {
+    add(nr: number): void;
+    getSum(): number;
 }
 
-class CharCounter{
-    constructor(protected adder:Adder){}
-    addWordCharacters(word:string):void{
+class CharCounter {
+    constructor(protected adder: Adder) {}
+
+    addWordCharacters(word: string): void {
         this.adder.add(word.length);
     }
-    getCharacterCount(){
+
+    getCharacterCount() {
         return this.adder.getSum();
     }
 }
 
-class SimpleAdder implements Adder{
-    protected sum:number=0;
-    add(nr:number){this.sum+=nr;}
+class CountingAdder implements Adder {
+    protected sum: number = 0;
+    protected count: number = 0;
+
+    add(nr: number) {
+        this.sum += nr;
+        this.count++;
+    }
+
     getSum(): number {
         return this.sum;
     }
+
+    getAverage() {
+        if (this.count > 0) {
+            return this.sum / this.count;
+        }
+        return 0;
+    }
 }
 
-let adder1:Adder=new SimpleAdder();
-let counter1:CharCounter=new CharCounter(adder1);
+let adder1: CountingAdder = new CountingAdder();
+let counter1: CharCounter = new CharCounter(adder1);
+
 counter1.addWordCharacters("Juku");
 counter1.addWordCharacters("tuli");
 counter1.addWordCharacters("kooli");
+
 console.log(counter1.getCharacterCount());
+console.log(adder1.getAverage());
